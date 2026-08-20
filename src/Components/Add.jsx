0143b1a3db3f5e3b5e3657c3fe04c727e361a608
Nikolawesly <nikolawesly@gmail.com>
2026-08-20 18:27:18 +0530

    First push of React app

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -16,20 +16,22 @@ export class Add extends Component {
   }
 
   submitHandler = (event) => {
-    event.preventDefault()//ensures result doesnt disappear in console
-    console.log(this.state)
+    event.preventDefault()
     fetch('http://localhost:3000/products', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(this.state)
     })
       .then(res => {
-        console.log(res)
+        console.log('Status:', res.status)
         if (res.status === 201) {
           alert('product added successfully')
           window.location.href = '/'
+        } else {
+          alert('Failed to add product. Status: ' + res.status)
         }
       })
+      .catch(err => alert('Error: ' + err.message))
   }
 
 
@@ -42,15 +44,15 @@ export class Add extends Component {
         <form action="" className='container' method='post' onSubmit={this.submitHandler} >
           <div className='row mb-3'>
             <div className='col' >
-              <label for="name" className='form-label' >Name</label>
-              <input type="text" className='form-control' id='name' name='name' defaultValue={name} onChange={this.ChangeHandler} />
+              <label htmlFor="name" className='form-label' >Name</label>
+              <input type="text" className='form-control' id='name' name='name' value={name} onChange={this.ChangeHandler} />
             </div>
           </div>
 
           <div className='row mb-3'>
             <div className='col' >
-              <label for="price" className='form-label' >Price</label>
-              <input type="text" className='form-control' id='price' name='price' defaultValue={price} onChange={this.ChangeHandler} />
+              <label htmlFor="price" className='form-label' >Price</label>
+              <input type="text" className='form-control' id='price' name='price' value={price} onChange={this.ChangeHandler} />
             </div>
           </div>
 
